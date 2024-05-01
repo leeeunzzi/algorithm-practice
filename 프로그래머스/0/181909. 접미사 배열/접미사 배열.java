69fbf3a776294073948c2eb7e03aa820b6560997
leeeunzzi <89902876+leeeunzzi@users.noreply.github.com>
2024-05-01 17:33:09 +0900

    [level 0] Title: 접미사 배열, Time: 0.17 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -1,18 +1,27 @@
 import java.util.*;
 class Solution {
     public String[] solution(String my_string) {
-        String substring = "";
-        ArrayList<String> resultList = new ArrayList<>();
-        for (int i = 0; i < my_string.length(); i++) {
-            substring = my_string.substring(i, my_string.length());
-            resultList.add(substring);
-            Collections.sort(resultList);
-        }
+//         String substring = "";
+//         ArrayList<String> resultList = new ArrayList<>();
+//         for (int i = 0; i < my_string.length(); i++) {
+//             substring = my_string.substring(i, my_string.length());
+//             resultList.add(substring);
+//             Collections.sort(resultList);
+//         }
 
-        String[] answer = new String[resultList.size()];
-        for (int i = 0; i < resultList.size(); i++) {
-            answer[i] = resultList.get(i);
+//         String[] answer = new String[resultList.size()];
+//         for (int i = 0; i < resultList.size(); i++) {
+//             answer[i] = resultList.get(i);
+//         }
+//         return answer;
+        
+        String[] answer = new String[my_string.length()];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = my_string.substring(i, my_string.length());
         }
+        Arrays.sort(answer);
+        
         return answer;
     }
 }
