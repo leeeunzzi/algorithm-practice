689a26c20a472758c0ba93c88739ddaafcce6c79
leeeunzzi <89902876+leeeunzzi@users.noreply.github.com>
2024-05-01 15:40:35 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.09 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -3,12 +3,7 @@ class Solution {
         int[] answer = {};
         
         for(int i=0; i<queries.length; i++) {
-            // queries[0] [0,3]
-            // queries[1] [1,2]
-            // queries[2] [1,4]
             for(int j=0; j<queries[i].length-1; j++) {
-                // queries[0][0] 0
-                // queries[0][1] 3
                 int temp = 0;
                 temp = arr[queries[i][j]];
                 arr[queries[i][j]] = arr[queries[i][j+1]];
