dfeb1184498f217a67b56469fda7312e9bc2aa00
leeeunzzi <89902876+leeeunzzi@users.noreply.github.com>
2024-05-08 10:54:14 +0900

    [level 0] Title: 문자열 뒤집기, Time: 1.10 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -2,6 +2,10 @@ import java.util.Arrays;
 
 class Solution {
     public String solution(String my_string, int s, int e) {
+        // StringBuilder answer = new StringBuilder(my_string.substring(s, e + 1));
+        // answer.reverse();
+        // return my_string.substring(0, s) + answer + my_string.substring(e + 1);
+        
         String answer = "";
         String[] text = my_string.split("");
         for (int i = s; i <= e; i++) {
