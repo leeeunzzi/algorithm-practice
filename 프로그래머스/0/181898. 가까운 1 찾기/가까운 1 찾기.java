b57a07d98923a4b0eaf322dd6aa040e7e6984811
leeeunzzi <89902876+leeeunzzi@users.noreply.github.com>
2024-05-09 11:52:08 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.01 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -2,10 +2,8 @@ class Solution {
     public int solution(int[] arr, int idx) {
         int answer = 0;
         for (int i = idx; i < arr.length; i++) {
-            if (i >= idx && arr[i] == 1) {
-                answer = i;
-                return answer;
-            } else answer = -1;
+            if (arr[i] == 1) return i;
+            else answer = -1;
         }
         return answer;
     }
